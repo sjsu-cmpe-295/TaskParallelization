a3bff552bfe21ac5aafd9804a23d9c6cd9a60f95
Clinton Rego <clinton.rego@sap.com>
2017-04-10 21:55:06 -0700

    Merge Changes in .gitignore

--- TaskParallelismFramework/TaskParallelism/src/sjsu/cmpe/B295/raspberrypi/node/TaskParallelizeApp.java
+++ TaskParallelismFramework/TaskParallelism/src/sjsu/cmpe/B295/raspberrypi/node/TaskParallelizeApp.java
@@ -1,6 +1,7 @@
 package sjsu.cmpe.B295.raspberrypi.node;
 
 import java.io.File;
+
 import org.slf4j.LoggerFactory;
 
 import sjsu.cmpe.B295.raspberrypi.node.RoutingConfig.RoutingEntry;
@@ -8,31 +9,25 @@ import sjsu.cmpe.B295.raspberrypi.node.RoutingConfig.RoutingEntry;
 import org.slf4j.Logger;
 
 public class TaskParallelizeApp {
-	protected static Logger logger = LoggerFactory.getLogger("TaskParallelizeApp");
-	    public static final String[] status = {"slave","master"};
+    protected static Logger logger = LoggerFactory.getLogger("TaskParallelizeApp");
+    public static final String[] status = {"slave", "master"};
 
 
-	public static void main(String[] args) {
-		logger.info("Task Parallelize App Started");
-		if (args.length == 0) {
-			logger.info("usage: server <config file>");
-			System.exit(1);
-		}
-
-		String configFilePath = args[0];
-		logger.info(configFilePath);
-		try {
-			Node node = new Node(configFilePath);
-			node.start();
+    public void start(String configFilePath) {
+        logger.info("Task Parallelize App Started");
+        logger.info("configFilePath " + configFilePath);
+        try {
+            Node node = new Node(configFilePath);
+            node.start();
 //			for(RoutingEntry entry: node.nodeState.routingConfig.routingEntries){
 //				logger.info(entry.getId()+":"+entry.getHost());
 //			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			logger.info("Task Parallelize App Closing");
-		}
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            logger.info("Task Parallelize App Closing");
+        }
 
-	}
+    }
 
 }
--- TaskParallelismFramework/springApi/src/main/java/com/example/Application.java
+++ TaskParallelismFramework/springApi/src/main/java/com/example/Application.java
@@ -1,12 +1,22 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sjsu.cmpe.B295.raspberrypi.node.TaskParallelizeApp;
 
 @SpringBootApplication
 public class Application {
+    protected static Logger logger = LoggerFactory.getLogger("Application");
 
     public static void main(String[] args) {
+        if (args.length == 0) {
+            logger.error("usage: server <config file>");
+            System.exit(1);
+        }
         SpringApplication.run(Application.class, args);
+        TaskParallelizeApp taskParallelizeApp = new TaskParallelizeApp();
+        taskParallelizeApp.start(args[0]);
     }
 }
